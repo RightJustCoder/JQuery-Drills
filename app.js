0cf5e9bd9edb2418639883e7daedd42b423714a1
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-20 15:41:24 -0500

    button alerts the value of the text input field

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-$('#btnSubmit').click(function() {
-    alert('#btnSubmit').val();
-})
+$(document).ready(function ()   {
+$('#btnSubmit').click(function () {
+    alert('It got clicked!!')
+    let txtValue = $('#txtHere').val();
+            alert(txtValue)
+        
+    });
+
+
+// DOM Content Closing Tag     
+});
+
+// $(this).removeProp('disabled')
+    //alert when there is something typed in the value
+
+// let txtValue = ('#txtHere').valueOf();
+// alert (txtValue);
+
