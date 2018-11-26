b1c3ec298625aa885f65e85a5a79e322bb4383bd
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-11-25 23:30:43 -0500

    Created Unordered list and appended it to the body. When the form is submitted the text is added to the ul. When the li element is clciked it changes to a color. When doubled clicked it is removed

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-$(document).ready(function () {
-  //Disables button 
-  $('#btnSubmit').prop('disabled', true)
+$(document).ready (function()   {
+    //Disables button 
+    $('#btnSubmit').prop('disabled', true)
 
-  //when letter is typed in the text area function  
-//   enables button
-  $('#txtHere').keyup (function()    {
-    $('#btnSubmit').prop('disabled', false)
-});
-
-    // Function when button clicked 
-    $('#btnSubmit').click(function () {
-        alert('It got clicked!!')
-        let txtValue = $('#txtHere').val();
-        alert(txtValue)
+    //when letter is typed in the text area function  
+    //enables button
+    $('#txtHere').keypress(function () {
+        $('#btnSubmit').prop('disabled', false)
 
     });
 
+    // Function when button clicked 
+    $('#btnSubmit').click(function () {
+       // Alert value of text input field when button clicked
+        let txtValue = $('#txtHere').val();
+        alert(txtValue)
 
-    //Create a Div Element and append it to the body
-    let div = document.createElement('div')
-    document.body.appendChild(div)
-    // DOM Content Closing Tag     
+        //Create a Div Element and append it to the body
+        $('body').append('<div id="myDiv"> </div>')
+        //Create Unordered list and append to body
+        $('body').append('<ul id ="myList"> </ul>')
+
+        //Append the text as list item to unordered list
+        li = $(`<li>${txtValue}</li>`)
+        $('#myList').append(li)
+        $('#myDiv').append($('#myList'))
+
+        
+
+        // list changes colors when clicked
+        li.click(function () {
+            let colorList = ['Green', 'Pink', 'Yellow', 'blue', 'black', 'brown', 'purple', 'red', 'orange'];
+            let randomIndex = Math.floor(Math.random() * colorList.length)
+            $(this).css('color', colorList[randomIndex])
+            
+            
+        });
+
+        // Doubleclicked list disappears
+        li.dblclick(function () {
+            $(this).remove();
+        });
+
+       
+        //End of btn submit function
+    });
+
+    // DOM Content Closing Tag   
 });
